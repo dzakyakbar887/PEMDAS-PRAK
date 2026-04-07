8b79bbcba908e5f9c1714a26c76bdc3fed0e7b8b
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-04-07 18:39:46 +0700

    langkah 16

---
+++ structdalamstruct.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct detailalamat;
+{
+    string desa;
+    string kota;
+};
+
+struct orang 
+{
+    string nama;
+    detailalamat alamat;
+    int umur;
+};
+
+int main ()
+{
+    //deklarasi variabel struct
+    orang mhs;
+    //mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+}

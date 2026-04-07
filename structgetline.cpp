378c935dd4a68ef6c5a4390cb06700c2431cefce
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-04-07 18:51:35 +0700

    langkah 18

---
+++ structgetline.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct detailalamat;
+{
+    string desa;
+    string kota;
+};
+
+struct orang 
+{
+    string nama;
+    detailalamat alamat;
+    int umur;
+};
+
+int main ()
+{
+    //deklarasi variabel struct
+    orang mhs;
+    //mengisi data
+    cout << "Isikan data nama : ";
+    getline(cin, mhs.nama);
+    cout << "Isikan data desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Isikan data kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+     cout << endl;
+
+    cout << "Data tampil" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
